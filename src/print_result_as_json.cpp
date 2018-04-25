5de00c188f8a68467b23fb3c7049efe7f031419b
White-Owl <White-Owl@users.noreply.github.com>
2018-04-25 11:20:54 -0400

    - Added proper support for JSON - Corrected Usage text

--- src/print_result_as_json.cpp
+++ src/print_result_as_json.cpp
@@ -1,47 +1,105 @@
 #include "nisql.h"
 
 void print_result_as_json(result &row) {
-	queue<string> file_names;
-	file_names.push(output_file_name);
-	if(options.find("SETS") != options.end()) {
+	queue<string> section_names;
+	queue<string> row_names;
+	string root_name = options["ROOT"];
+	if(root_name.empty()) root_name = "root";
+	bool should_format = ! is_format_option_on("STREAM");
+	bool expect_multiset = options.find("SETS") != options.end();
+	bool each_column_at_line = is_format_option_on("FIELDPERLINE");
+
+	if(expect_multiset) {
 		istringstream ss(options["SETS"]);
 		string next_name;
 		while( getline(ss, next_name, ',') ){
-			file_names.push(next_name);
+			section_names.push(next_name);
+		}
+	}
+	if(options.find("ROWS") != options.end()) {
+		istringstream ss(options["ROWS"]);
+		string next_name;
+		while( getline(ss, next_name, ',') ){
+			row_names.push(next_name);
 		}
 	}
 
+	ofstream ofs(output_file_name);
+	ofs << "{ \"" << root_name << "\": [";
+	//if(should_format) ofs << "\n";
 
-	int file_count = 0;
+	int section_count = 0;
 	do {
-		string file_name;
-		if(file_names.empty()) {
-			file_name = output_file_name.substr(0, output_file_name.size()-4) + '_' + to_string(file_count) + ".json";
-		} else {
-			file_name = file_names.front();
-			file_names.pop();
-		}
-		if(verbose) cout << "Resultset #" << file_count << " into " << file_name << "\n";
-		ofstream ofs(file_name);
+		string section_name;
+		string row_name = "row";
+		if(expect_multiset || section_count>0) {
+			if(section_names.empty()) {
+				section_name = "section_" + to_string(section_count);
+			} else {
+				section_name = section_names.front();
+				section_names.pop();
+			}
+			if(verbose) cout << "Resultset #" << section_count << " into " << section_name << "\n";
+			if (section_count > 0) {
+				ofs << ", ";
+			}
+			if (should_format) ofs << "\n";
 
-		ofs << "{ \"result\": ";
+			if(should_format) ofs << "\t";
+
+			ofs << " {\"" << section_name << "\": ["; //check if closed
+			//if(should_format) ofs << "\n";
+		}
+
+		if(! row_names.empty()) {
+			row_name = row_names.front();
+			row_names.pop();
+		}
+
+
 		unsigned long row_count=0;
 		while( row.next() ) {
-			if (row_count > 0) ofs << ",\n";
-			ofs << "[ ";
-			for(auto col_idx=0; col_idx<row.columns(); col_idx++) {
-				if(col_idx>0) ofs << ", "; // separator between columns
-				ofs << row.column_name(col_idx) << ':' << '"' << row.get<nanodbc::string>(col_idx) << '"';
+			if (row_count > 0) ofs << ",";
+			if(should_format) {
+				ofs << "\n\t";
+				if(expect_multiset || section_count>0) ofs << "\t";
 			}
-			ofs << "] ";
+			/*if (row_name == "row") {
+				row_name = "row_"+to_string(row_count);
+			}*/
+			ofs << "{ \"" << row_name << "\": {";
+			if(should_format && each_column_at_line) {
+				for(auto col_idx=0; col_idx<row.columns(); col_idx++) {
+					if (col_idx > 0) ofs << ',';
+					ofs << "\n\t\t";
+					if(expect_multiset || section_count>0) ofs << "\t";
+					ofs << '"' << row.column_name(col_idx) << "\":";
+					ofs << '"' << row.get<nanodbc::string>(col_idx) << '"';
+				}
+				ofs << "\n\t";
+				if(expect_multiset || section_count>0) ofs << "\t";
+				ofs << "}"; //end row
+			} else {
+				for(auto col_idx=0; col_idx<row.columns(); col_idx++) {
+					if (col_idx > 0) ofs << ',';
+					ofs << '"' << row.column_name(col_idx) << ':' << '"' << row.get<nanodbc::string>(col_idx) << '"';
+				}
+				ofs << "} "; //end row
+			}
 			row_count++;
 		}
-		ofs << "} ";
-		ofs.close();
+		if(expect_multiset || section_count>0) {
+			if(should_format) ofs << "\t";
+			ofs << "] }"; //ends section name
+			if(should_format) ofs << "\n";
+		}
+
 		if(verbose) cout << row_count << " rows written\n";
 
 
-		file_count++;
+		section_count++;
 	} while (row.next_result());
+	ofs << " ]}";
+	ofs.close();
 }
 
--- tests/test8.sh
+++ tests/test8.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # MySQL's pecularity: Option=67108864 required for multiple sets
 cd ..
-src/nisql "DSN=mysql1;UID=jim;PWD=123;Option=67108864" "select * from student" out1.json -v
-src/nisql "DSN=mysql1;UID=jim;PWD=123;Option=67108864" "select * from student" out2.json -v -f"???"
-src/nisql "DSN=mysql1;UID=jim;PWD=123;Option=67108864" "select * from student; select * from class" out3.json -v
-src/nisql "DSN=mysql1;UID=jim;PWD=123;Option=67108864" "select * from student; select * from class" out4.json -v -f"???"
+con="DSN=mysql1;UID=jim;PWD=123;Option=67108864"
+src/nisql $con "select * from student" out1.json -v -f"root=students"
+src/nisql $con "select * from student" out2.json -v -f"rows=student"
+src/nisql $con "select * from student" out3.json -v -f"root=students;rows=student"
+src/nisql $con "select * from student; select * from class" out4.json -v
+src/nisql $con "select * from student; select * from class" out5.json -v -f"SETS=student,class;ROOT=data"
+src/nisql $con "select * from student; select * from class" out6.json -v -f"sets=students,classes;rows=student,class;root=data;stream=yes"
+src/nisql $con "select * from student; select * from class" out7.json -v -f"sets=students,classes;rows=student,class;root=data;stream=no"
+src/nisql $con "select * from student; select * from class" out8.json -v -f"sets=students,classes;rows=student,class;root=data;FieldPerLine=yes"
+
 
